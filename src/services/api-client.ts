4f1ab8619499bc186fab0e512cedcb1c52253116
Shane Cummings <shanecummings_uk@yahoo.co.uk>
2026-01-22 19:05:42 +0000

    feat: update apiClient to use environment variables for base URL and API key

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-    baseURL: 'https://api.rawg.io/api',
-    params: {
-        key: '10a98ccdc35b40b2b03f6aaf9651ad4c'
-    },
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  params: {
+    key: import.meta.env.VITE_API_KEY,
+  },
 });
 
 export default apiClient;
