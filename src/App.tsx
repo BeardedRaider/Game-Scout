b8001c8360dcdb27396861ed2dd17ea3a56cd649
Shane Cummings <shanecummings_uk@yahoo.co.uk>
2026-01-22 15:58:02 +0000

    responsive layout tested

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
-import { Button } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
-  return <Button colorScheme="blue">Click me</Button>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`, //lrg devices more than 1024px
+      }}
+    >
+      {/* Your components will go here */}
+      <GridItem area="nav" bg="Coral">
+        Navigation Bar
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside" bg="LightBlue">
+          Aside Section
+        </GridItem>
+      </Show>
+      <GridItem area="main" bg="LightGreen">
+        Main Content
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
