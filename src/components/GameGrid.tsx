d5e6240393a871cac03f311209e2ad06cbfd145c
Shane Cummings <shanecummings_uk@yahoo.co.uk>
2026-01-23 11:18:57 +0000

    feat: implement GameCard component for displaying individual game details and refactor GameGrid to utilize it

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,5 +1,6 @@
-import { Text } from "@chakra-ui/react";
+import { SimpleGrid, Text } from "@chakra-ui/react";
 import useGames from "../hooks/useGames";
+import GameCard from "./GameCard";
 // Define TypeScript interfaces for the game data
 
 
@@ -12,13 +13,13 @@ const GameGrid = () => {
     // This fragment displays either an error or the list of games
     <>
       {error && <div>Error: {error}</div>}
-      <ul> 
+      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} padding={10} spacing={10}> 
         {/* // Map through the games and display their names */}
         {games.map((game) => (
             // Each game is rendered as a list item with a unique key
-          <li key={game.id}>{game.name}</li>
+          <GameCard key={game.id} game={game} />
         ))}
-      </ul>
+      </SimpleGrid>
     </>
   );
 };
