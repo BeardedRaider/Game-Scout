5ccc3160dccb6863c7238e3e62b72ce0dd27674e
Shane Cummings <shanecummings_uk@yahoo.co.uk>
2026-02-02 21:03:45 +0000

    fix: improve error handling in GameGrid component by displaying error message and cleaning up JSX structure

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,4 +1,4 @@
-import { SimpleGrid } from "@chakra-ui/react";
+import { SimpleGrid, Text } from "@chakra-ui/react";
 import useGames, { type Game, type Platform } from "../hooks/useGames";
 import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
@@ -7,8 +7,7 @@ import type { GameQuery } from "@/App";
 // Define TypeScript interfaces for the game data
 
 interface Props {
-  gameQuery: GameQuery
-
+  gameQuery: GameQuery;
 }
 
 // Functional component to display a grid of games
@@ -16,32 +15,31 @@ const GameGrid = ({ gameQuery }: Props) => {
   const { data, error, isLoading } = useGames(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6]; // Create an array of 6 skeletons
 
+  if (error) return <Text>{error}</Text>;
+
   return (
     // This fragment displays either an error or the list of games
-    <>
-      {error && <div>Error: {error}</div>}
-      <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
-        padding={"10px"}
-        spacing={6}
-      >
-        {/* Game card container this is for loading state */}
-        {isLoading &&
-          skeletons.map((skeleton) => (
-            // Render a GameCardSkeleton for each item in the skeletons array
-            <GameCardContainer key={skeleton}>
-              <GameCardSkeleton  />
-            </GameCardContainer>
-          ))}
-        {/* // Map through the games and display their names */}
-        {data.map((game) => (
-          // Each game is rendered as a list item with a unique key
-          <GameCardContainer key={game.id}>
-            <GameCard game={game} />
+    <SimpleGrid
+      columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+      padding={"10px"}
+      spacing={6}
+    >
+      {/* Game card container this is for loading state */}
+      {isLoading &&
+        skeletons.map((skeleton) => (
+          // Render a GameCardSkeleton for each item in the skeletons array
+          <GameCardContainer key={skeleton}>
+            <GameCardSkeleton />
           </GameCardContainer>
         ))}
-      </SimpleGrid>
-    </>
+      {/* // Map through the games and display their names */}
+      {data.map((game) => (
+        // Each game is rendered as a list item with a unique key
+        <GameCardContainer key={game.id}>
+          <GameCard game={game} />
+        </GameCardContainer>
+      ))}
+    </SimpleGrid>
   );
 };
 
