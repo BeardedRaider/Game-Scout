f35f6dda510cb83a6183eedef88602d06a395608
Shane Cummings <shanecummings_uk@yahoo.co.uk>
2026-03-01 23:05:12 +0000

    feat: restructure GameDetailPage layout to use SimpleGrid for improved display of game details and media
    (May tweak and adjust later)

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -3,7 +3,7 @@ import GameAttibutes from "@/components/GameAttibutes";
 import GameScreenShots from "@/components/GameScreenShots";
 import GameTrailer from "@/components/GameTrailer";
 import useGame from "@/hooks/useGame";
-import { Heading, Spinner } from "@chakra-ui/react";
+import { Box, Heading, SimpleGrid, Spinner } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
 
 const GameDetailPage = () => {
@@ -15,13 +15,21 @@ const GameDetailPage = () => {
   if (error || !game) throw error;
 
   return (
-    <>
-      <Heading>{game?.name}</Heading>
-      <ExpandText>{game?.description_raw}</ExpandText>
-      <GameAttibutes game={game} />
+    <SimpleGrid columns={{ base: 1, md: 2}} spacing={4}>
+      {/* //left column, will be used for game details */}
+      <Box>
+        <Heading>{game?.name}</Heading>
+        <ExpandText>{game?.description_raw}</ExpandText>
+        <GameAttibutes game={game} />
+      </Box>
+
+      {/* //right column, will be used for trailers and screenshots */}
+      <Box> 
       <GameTrailer gameId={game.id} />
       <GameScreenShots gameId={game.id} />
-    </>
+
+      </Box>
+    </SimpleGrid >
   );
 };
 
