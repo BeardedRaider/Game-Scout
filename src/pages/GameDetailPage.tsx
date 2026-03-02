89d5d556ef217a3d5202c00440b9cb0f42976211
Shane Cummings <shanecummings_uk@yahoo.co.uk>
2026-03-02 06:12:07 +0000

    refactor: streamline GameDetailPage layout and improve metadata display with CriticScore integration
    Hope to fix a problem with aspectRation not working with ios and breaking the app

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,14 +1,12 @@
+import CriticScore from "@/components/CriticScore";
 import ExpandText from "@/components/ExpandText";
 import GameAttibutes from "@/components/GameAttibutes";
 import GameScreenShots from "@/components/GameScreenShots";
 import GameTrailer from "@/components/GameTrailer";
-import useGame from "@/hooks/useGame";
-import { Box, Heading, SimpleGrid, Spinner } from "@chakra-ui/react";
-import { useParams } from "react-router-dom";
-import { AspectRatio } from "@chakra-ui/react";
-import { Tag, TagLabel, HStack, Wrap, WrapItem } from "@chakra-ui/react";
-import CriticScore from "@/components/CriticScore";
 import PlatformIconList from "@/components/PlatformIconList";
+import useGame from "@/hooks/useGame";
+import { Box, Heading, HStack, SimpleGrid, Spinner, Tag, TagLabel, Wrap, WrapItem } from "@chakra-ui/react";
+import { useParams } from "react-router-dom";
 
 const GameDetailPage = () => {
   const { slug } = useParams();
@@ -21,27 +19,30 @@ const GameDetailPage = () => {
   return (
     <Box>
       {/* HERO BANNER */}
-      <AspectRatio ratio={16 / 6} mb={6}>
-        <Box position="relative" borderRadius={10} overflow="hidden">
-          {/* Background image + gradient overlay to make text more readable */}
-          <Box
-            position="absolute"
-            inset={0} // inset 0 means top: 0, right: 0, bottom: 0, left: 0
-            backgroundImage={`linear-gradient(to bottom, 
-            rgba(0,0,0,0.3), 
-            rgba(0,0,0,0.7)), 
-            url(${game.background_image})`}
-            backgroundSize="cover"
-            backgroundPosition="center"
-          />
-          {/* Title overlay */}
-          <Box position="absolute" bottom={4} left={4} color="white" zIndex={1}>
-            <Heading size={{ base: "lg", md: "xl", lg: "2xl" }}>
-              {game.name}
-            </Heading>
-          </Box>
+      <Box
+        position="relative"
+        height={{ base: "200px", md: "300px", lg: "350px" }}
+        borderRadius={10}
+        overflow="hidden"
+        mb={6}
+      >
+        <Box
+          position="absolute"
+          inset={0}
+          backgroundImage={`linear-gradient(to bottom,
+      rgba(0,0,0,0.3),
+      rgba(0,0,0,0.7)
+    ), url(${game.background_image})`}
+          backgroundSize="cover"
+          backgroundPosition="center"
+        />
+
+        <Box position="absolute" bottom={4} left={4} color="white" zIndex={1}>
+          <Heading size={{ base: "lg", md: "xl", lg: "2xl" }}>
+            {game.name}
+          </Heading>
         </Box>
-      </AspectRatio>
+      </Box>
 
       <Box mb={4}>
         {/* DESKTOP metadata tags section for genres, platforms, etc. */}
@@ -64,7 +65,7 @@ const GameDetailPage = () => {
             size={8}
           />
           <Box textAlign="right">
-            <HStack spacing={2} justify="flex end" align="center">
+            <HStack spacing={2} justify="flex-end" align="center">
               <Box fontSize="sm" fontWeight="bold" color="gray.200">
                 Critic Score
               </Box>
@@ -85,11 +86,14 @@ const GameDetailPage = () => {
             />
 
             <Box textAlign="right">
-              <HStack spacing={2} justify="flex end" align="center">
+              <HStack spacing={2} justify="flex-end" align="center">
                 <Box fontSize="sm" fontWeight="bold" color="gray.200">
                   Rating
                 </Box>
-                <CriticScore score={game.metacritic} reviewsCount={game.reviews_count} />
+                <CriticScore
+                  score={game.metacritic}
+                  reviewsCount={game.reviews_count}
+                />
               </HStack>
             </Box>
           </HStack>
